feb6269d19637ea35b885230c8f3e57fe37289df
ramiKang <harami0007@naver.com>
2023-10-29 15:57:47 +0900

    redux subscribe

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span")
 
+number.innerText = 0
+
 const countModifier = (count=0,action) =>{
+  console.log(count,action)
   if (action.type === "add"){
     return count +1;
   }else if(action.type === "minus"){
@@ -18,12 +21,19 @@ const countModifier = (count=0,action) =>{
 
 const countStore = createStore(countModifier)
 
-countStore.dispatch({type:"add"})
-countStore.dispatch({type:"add"})
-countStore.dispatch({type:"add"})
-countStore.dispatch({type:"add"})
-countStore.dispatch({type:"add"})
-countStore.dispatch({type:"add"})
-countStore.dispatch({type:"minus"})
+const onChange = () =>{
+  number.innerText = countStore.getState();
+}
 
-console.log(countStore.getState())
+countStore.subscribe(onChange)
+
+const handleAdd = () =>{
+  countStore.dispatch({type:"add"})
+}
+
+const handleMinus = () =>{
+  countStore.dispatch({type:"minus"})
+}
+
+add.addEventListener("click",handleAdd)
+minus.addEventListener("click",handleMinus)
